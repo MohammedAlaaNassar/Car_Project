e49578b93aff6b85682dedf83e3dcb92594bc13f
Ahmed__Ehab <ahmedehab11@gmail.com>
2019-09-25 19:15:58 +0200

    edit comments

--- reg_lib.h
+++ reg_lib.h
@@ -13,8 +13,10 @@
 #include "std_types.h"
 
 
-
+#define  F_CPU	1000000
 #define MAX_NUMBER_OF_PINS (uint8)32
+
+#include <avr/io.h>
 
 #define MAPPING(REG)  ( *( volatile uint8*) REG )
 #define MAPPING16(REG)  ( *( volatile uint16*) REG )
@@ -28,7 +30,8 @@
 #define PIN6                6
 #define PIN7                7
 
-/* Status Register */
+/*
+// Status Register //
 
 #define SREG     MAPPING(0x5F)
 
@@ -42,40 +45,40 @@
 #define   Z    1
 #define   C    0
 
-/* I/O registers */
+// I/O registers //
 
-/******************* PORTS *******************/
+///////////////////// PORTS ////////////////////
 
-/* Port D */
+// Port D //
 #define PIND     MAPPING(0x30)
 #define DDRD     MAPPING(0x31)
 #define PORTD    MAPPING(0x32)
 
 // Bit numbers
 
-/* Port C */
+//--------- Port C ------------/
 #define PINC     MAPPING(0x33)
 #define DDRC     MAPPING(0x34)
 #define PORTC    MAPPING(0x35)
 
 // Bit numbers
 
-/* Port B */
+//--------- Port B ------------/
 #define PINB     MAPPING(0x36)
 #define DDRB     MAPPING(0x37)
 #define PORTB    MAPPING(0x38)
 
 // Bit numbers
 
-/* Port A */
+//--------- Port A ------------/
 #define PINA     MAPPING(0x39)
 #define DDRA     MAPPING(0x3A)
 #define PORTA    MAPPING(0x3B)
 
 
-/******************* Timers *******************/
+/-------------- Timers ------------/
 
-/* TIMSK */
+//--------- TIMSK ------------/
 #define OCIE2   7
 #define TOIE2   6
 #define TICIE1  5
@@ -85,7 +88,7 @@
 #define OCIE0   1
 #define TOIE0   0
 
-/* TIFR */
+//-------- TIFR ----------//
 #define OCF2    7
 #define TOV2    6
 #define ICF1    5
@@ -95,7 +98,7 @@
 #define OCF0    1
 #define TOV0    0
 
-/* Timer 0 */
+//----- Timer 0 ------//
 #define TCNT0   MAPPING(0x52)
 #define TCCR0   MAPPING(0x53)
 
@@ -115,7 +118,7 @@
 
 #define OCR0    MAPPING(0x5C)
 
-/* TCCR0 */
+//------ TCCR0 -------- //
 #define FOC0    7
 #define WGM00   6
 #define COM01   5
@@ -126,12 +129,12 @@
 #define CS00    0
 
 
-/* Timer 2 */
+//------- Timer 2 -------- //
 #define OCR2    MAPPING(0x43)
 #define TCNT2   MAPPING(0x44)
 #define TCCR2   MAPPING(0x45)
 
-/* TCCR2 */
+//-------  TCCR2 -------- //
 #define FOC2    7
 #define WGM20   6
 #define COM21   5
@@ -141,7 +144,7 @@
 #define CS21    1
 #define CS20    0
 
-/* Timer 1 */
+//-------  Timer 1 -------- //
 #define ICR1    MAPPING16(0x46)
 #define ICR1L   MAPPING(0x46)
 #define ICR1H   MAPPING(0x47)
@@ -159,7 +162,7 @@
 
 
 
-/* TCCR1A */
+//-------  TCCR1A -------- //
 #define COM1A1  7
 #define COM1A0  6
 #define COM1B1  5
@@ -169,15 +172,15 @@
 #define WGM11   1
 #define WGM10   0
 
-/* TCCR1B */
+//-------  TCCR1B -------- //
 #define ICNC1   7
 #define ICES1   6
-/* bit 5 reserved */
+//-------  bit 5 reserved -------- //
 #define WGM13   4
 #define WGM12   3
 #define CS12    2
 #define CS11    1
 #define CS10    0
-
+*/
 
 #endif /* REG_LIB_H_ */
