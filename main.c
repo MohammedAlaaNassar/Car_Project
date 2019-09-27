2c69aa9ededa477aa97573d4233e96611def527b
Ahmed__Ehab <ahmedehab11@gmail.com>
2019-09-27 23:49:27 +0200

    Final Final Main Program :D

--- main.c
+++ main.c
@@ -13,6 +13,102 @@
 #include "Timer_Draft.h"
 #include "7_SEG.h"
 
+//define constants
+#define ANTICLOCKWISE 1
+#define CLOCKWISE	  0
+#define ENABLE		  1
+#define UNABLE	      0
+
+#define M1_IN1 PD0				// IN1 is supplied by logic 1
+#define M1_IN2 PD1				// IN2 is supplied by logic 0
+#define M1_EN1 PD5
+#define DDR_MOTOR_IN1   DDRD
+#define PORT_MOTOR_IN1  PORTD
+#define DDR_MOTOR_IN2   DDRD
+#define PORT_MOTOR_IN2  PORTD
+#define DDR_MOTOR_EN1   DDRD
+#define PORT_MOTOR_EN1  PORTD
+
+ISR(TIMER0_COMP_vect)
+{
+	PORTC |= (1<<5);
+	/*
+	OVF_Counter_Loop--;
+	
+	if(OVF_Counter_Loop == 0)
+	{
+		Running_Flag[TIMER0]=1;
+	}*/
+	DIO_write(PWM_PORT,PWM_PIN,va);
+	va^=1;
+		TIMSK &= ~(1<<OCIE0);
+}
+
+ISR(TIMER0_OVF_vect)
+{
+	PORTC |= (1<<6);
+	/*
+	OVF_Counter_Loop--;
+	
+	if(OVF_Counter_Loop == 0)
+	{
+		Running_Flag[TIMER0]=1;
+	}*/
+	
+	DIO_write(PWM_PORT,PWM_PIN,va);
+	va^=1;
+		TIMSK |= (1<<OCIE0);
+	
+}
+
+
+void MOTORS_Initialization (void)
+{
+	DDR_MOTOR_IN1 |= (1<<M1_IN1) ;  // enable the IN1 of the Half Bridge as an output
+	DDR_MOTOR_IN2 |= (1<<M1_IN2) ;  // enable the IN2 of the Half Bridge as an output
+	
+	DDR_MOTOR_EN1 |= (1<<M1_EN1) ;  // enable the EN1 of the Half Bridge as an output
+	
+	
+}
+
+void MOTORS_ENABLE ( unsigned char Status)
+{
+	
+	if (Status == ENABLE)
+	{
+		PORT_MOTOR_EN1 |= (1<<M1_EN1) ; // Put logic one to the EN1 of the Half Bridge
+	}
+	
+	else if (Status == UNABLE)
+	{
+		PORT_MOTOR_EN1 &= ~(1<<M1_EN1) ; // Put logic zero to the EN1 of the Half Bridge
+	}
+
+	
+}
+
+
+void MOTOR_Direction ( unsigned char Set_Direction)
+{
+	if( Set_Direction == ANTICLOCKWISE)  // Default Direction
+	{
+		
+		PORT_MOTOR_IN2 &= ~(1<<M1_IN2) ; // Put logic zero on the IN2 to the half bridge (L298)
+		PORT_MOTOR_IN1 |= (1<<M1_IN1) ;  // Put logic one on the IN1 to the half bridge (L298)
+
+	}
+	
+	else if( Set_Direction == CLOCKWISE)
+	{
+		PORT_MOTOR_IN1 &= ~ (1<<M1_IN1) ;  // Put logic one on the IN1 to the half bridge (L298)
+		PORT_MOTOR_IN2 |= (1<<M1_IN2) ; // Put logic zero on the IN2 to the half bridge (L298)
+
+	}
+	
+}
+
+
 
 int main(void)
 {	
@@ -21,32 +117,50 @@ int main(void)
 	
 	test= TIMER_init();
 	
-	DDRC=0xFF;
-	DDRA=0xFF;
-	DDRD=0xFF;
-	DDRB=0xFF;
 	
-	uint8 value=1;
 	
 	// this code line below is used to check WGM_CTC_mode in TOGGLE_Com_mode in TIMER1 and it works
-	test = Time_Delay(timer0, 0.2);
+	//test = Time_Delay(timer0, 0.2);
 	
 
 	
 	// this code line below is used to check WGM_CTC_mode in TOGGLE_Com_mode in TIMER1 and it works
-	test=Time_Delay(TIMER1, (0.2) );
+	//test=Time_Delay(TIMER1, (0.2) );
 	
 	// We need to test  WGM_Normal_mode in TOGGLE_Com_mode in both timers 
 	
 	
-	test = DIO_write(PORT_D , PIN5 , LOW);
+	DIO_write(PWM_PORT,PWM_PIN,va);
+
+	PWM_generate(TIMER0,50,NON_INVERTING);
 	
+	DDRD=0xFF;
+	
+	PORTD |= (1<<4);
+	MOTORS_Initialization ();
+	MOTORS_ENABLE(ENABLE);
+	MOTOR_Direction(CLOCKWISE);
+	for(int i=0;i<10;i++)
+	{	
+	 SEG_write(i);
+		Time_Delay(timer1,1);
+			while( (TIFR & (1<<OCF1A)) == 0);
+			{ TIFR |= 1<<OCF1A ;}
+	}	
+		MOTOR_Direction(ANTICLOCKWISE);	
+		for(int i=0;i<10;i++){
+			SEG_write(i);
+			Time_Delay(timer1,1);
+			while( (TIFR & (1<<OCF1A)) == 0);
+			{ TIFR |= 1<<OCF1A ;}
+		}
+		MOTORS_ENABLE(UNABLE);
+		
+			
 	while (1)
 	{
-		/*
-		// these code lines below is used to check WGM_Normal_mode in Normal_Com_mode in TIMER0 by polling on TOV0 flag and it needs testing
-		
-		test=Time_Delay(TIMER1, (0.2) );		
+		// these code lines below is used to check WGM_Normal_mode in Normal_Com_mode in TIMER0 by polling on TOV0 flag and it needs testing	
+	/*	test=Time_Delay(TIMER1, (0.2) );		
 		while( (TIFR & (1<<TOV0)) == 0);
 		{ TIFR |= 1<<TOV0 ;}		
 		DIO_write(PORT_B , PIN3 , value);
@@ -80,8 +194,7 @@ int main(void)
 		// these code lines below is used to check WGM_Normal_mode in Normal_Com_mode in TIMER1 by polling on TOV1 flag and it works
 		
 		test=Time_Delay(TIMER1, (0.2) );		
-		while( (TIFR & (1<<TOV1)) == 0);
-		{ TIFR |= 1<<TOV1 ;}		
+		
 		DIO_write(PORT_D , PIN5 , value);
 		value ^=1;
 		*/
