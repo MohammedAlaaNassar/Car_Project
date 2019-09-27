3876dba20ad441bd388ad222a6b7e4a2e9c27ead
Mohammed Nassar <mohammedalaanassar@hotmail.com>
2019-09-27 17:06:40 +0200

    Mohammed Nassar - My ADDS

--- TIMER_cnfg.h
+++ TIMER_cnfg.h
@@ -16,6 +16,40 @@ typedef enum {timer0,timer1,timer2,NUM_OF_TIMERS}TIMER_t; // types
 
 #define MAX_NUM_OF_TIMERS 3
 
+
+//----------------------------------- TIIMER_PINS ------------------------------------//
+
+//Define Output Compare Pin 1A and 1B and Input Capture Register for Timer 1:-
+
+#define DDR_TIMER_1		DDRD
+#define PORT_TIMER_1	PORTD
+#define PIN_TIMER_1		PIND
+
+#define OC1B	PD4
+#define OC1A	PD5
+#define ICP		PD6
+
+
+#define DDR_ICU	 DDRD
+#define PORT_ICU PORTD
+#define PIN_ICU	 PIND
+
+//Define Output Compare Pin for Timer 0:-
+
+#define DDR_TIMER_0		DDRB
+#define PORT_TIMER_0	PORTB
+#define PIN_TIMER_0		PINB
+#define OC0				PB3
+
+
+//Define Output Compare Pin for Timer 2:-
+#define DDR_TIMER_2		DDRD
+#define PORT_TIMER_2	PORTD
+#define PIN_TIMER_2		PIND
+#define OC2				PD7
+
+
+
 /* TIMER # */
 #define TIMER0    0
 #define TIMER1    1
@@ -55,13 +89,19 @@ typedef enum {timer0,timer1,timer2,NUM_OF_TIMERS}TIMER_t; // types
 
 /* ICU */
 #define ICU_USED     1
-#define ICU_NO_USED  0
+#define NO_ICU_USED  0
 
 #define NA           0xff
 
 /* INITIALIZATION	 */
 #define INITIALIZED			1
 #define NOT_INITIALIZED		0
+
+//Flag PWM
+#define PWM_MODE		1
+#define NON_PWM_MODE	0
+
+
 
 
 
@@ -72,10 +112,9 @@ typedef struct
   uint8 COM_mode;
   uint8 interrupt;
   uint8 ICU;
-  uint8 IS_init;
   uint8 prescalar;
+  uint8 IS_init;
 } TIMER_cnfg_t ;
-
 
 extern TIMER_cnfg_t TIMER_cnfg_arr[NUM_OF_TIMERS];
 
--- Timer_Draft.h
+++ Timer_Draft.h
@@ -11,11 +11,9 @@
 #include "TIMER_cnfg.h"
 
 
-
-
 ACK TIMER_init(void);
-ACK Time_Delay ( TIMER_t Timer_Select , float Required_Delay  );
+ACK Time_Delay ( TIMER_t Timer_Select , double Required_Delay  );
 ACK TIMER_Start ( TIMER_t Timer_Select );
-
+ACK PWM_generate( TIMER_t Timer_Select , uint8 Required_duty_cycle ,uint8 );
 
 #endif /* TIMER_H_ */
